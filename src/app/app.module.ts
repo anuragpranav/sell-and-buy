3ca6b5741dad5e2ed4bc87b924180aad652c6c15
anuragpranav <anurag92pranav@gmail.com>
2023-06-04 21:30:54 +0530

    Created component - new, edit, detail, contact.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingPageComponent } from './listing-page/listing-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListingPageComponent
+    ListingPageComponent,
+    ListingDetailPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
