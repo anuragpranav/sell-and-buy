39c1dc9c95101cadd03c03af15799536cd47196d
anuragpranav <anurag92pranav@gmail.com>
2023-06-26 21:18:33 +0530

    Add fake data.

--- src/app/listing-page/listing-page.component.ts
+++ src/app/listing-page/listing-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data'
+
 
 @Component({
   selector: 'app-listing-page',
   templateUrl: './listing-page.component.html',
   styleUrls: ['./listing-page.component.css']
 })
-export class ListingPageComponent {
+export class ListingPageComponent implements OnInit {
+
+  listings: Listing[] = [];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 
 }
