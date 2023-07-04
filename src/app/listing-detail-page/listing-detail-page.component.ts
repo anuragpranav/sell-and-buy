88769a957e8b5beebb2118fd5aa78935bc656247
anuragpranav <anurag92pranav@gmail.com>
2023-07-04 21:48:55 +0530

    Listing page component

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
   templateUrl: './listing-detail-page.component.html',
   styleUrls: ['./listing-detail-page.component.css']
 })
-export class ListingDetailPageComponent {
 
+export class ListingDetailPageComponent implements OnInit{ 
+
+  listing: Listing;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    const fakeListing = fakeListings.find(listing => listing.id === id);
+
+    if (fakeListing !== undefined){
+      this.listing = fakeListing;
+    }
+  }
 }
